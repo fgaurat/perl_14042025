4632f9ff3fe7b51236267739c5f0a9d77bfc400e
Frédéric GAURAT <fgaurat@gmail.com>
2025-04-16 14:24:45 +0200

    add module

---
+++ 17_use_utils.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use Utils;
+# use Utils qw(say_hello);
+
+sub main{
+    my $n = "fred";
+    Utils::say_hello($n);
+
+}
+
+main(@ARGV);
+
---
+++ Utils.pm
@@ -0,0 +1,14 @@
+package Utils;
+use strict;
+use warnings;
+
+sub say_hello{
+    my ($name) = @_;
+
+    print "Bonjour $name";
+
+}
+
+
+
+1;
